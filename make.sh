f486da2caebe747f4f58efe0b2d29845b515cd1f
Cyrille Lavigne <clavigne@chem.utoronto.ca>
2021-06-21 14:17:00 -0400

    Significant performance improvements to the fortran version

--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-f2py3 -c -m mytest test-f2py.f90 --f90flags='-O3 -march=native -funroll-loops -fopenmp -ffast-math -floop-nest-optimize' -lgomp
+f2py3 -c -m mytest test-f2py.f90 --f90flags='-lmkl -O3 -march=native -funroll-loops -fopenmp -ffast-math -floop-nest-optimize' -lgomp
