b8e48e873f6844f918041881b89ea9df7ac2a41a
mascondosa <martin.d.maas@gmail.com>
2021-06-24 20:46:35 -0400

    added LoopVectorization version

--- test_loopvec.jl
+++ test_loopvec.jl
@@ -1,25 +1,11 @@
 using LoopVectorization: @tvectorize
 
-function eval_exp(N)
-    a = range(0, stop=2*pi, length=N)
-    A = Matrix{ComplexF64}(undef, N, N)
-
-    @inbounds Threads.@threads for j in 1:N
-    for i in 1:N
-        A[i,j] = exp((100+im)*im*sqrt(a[i]^2 + a[j]^2))
-    end
-    end
-    
-    return A
-
-end
-
 function eval_exp_tvectorize(N)
     a = range(0, stop=2*pi, length=N)
     A = Matrix{ComplexF64}(undef, N, N)
 
     _A = reinterpret(reshape, Float64, A)
-    @tvectorize for j in 1:N, i in 1:N
+    @avs thread=true @tvectorize for j in 1:N, i in 1:N
         x = sqrt(a[i]^2 + a[j]^2)
         prefac = exp(-x)
         s, c = sincos(100*x)
@@ -30,13 +16,9 @@ function eval_exp_tvectorize(N)
     return A
 end
 
-eval_exp(5)
+eval_exp_tvectorize(5)
 print(string("running loop on ", Threads.nthreads(), " threads \n"))
 for N in 1000:1000:10000
-    @time begin
-    A = eval_exp(N)
-    end
-    
     @time begin
     A = eval_exp_tvectorize(N)
     end
